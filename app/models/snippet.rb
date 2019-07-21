18bb5c98d062e67a0767c08f66a8a765c0ea37bd
petitviolet <violethero0820@gmail.com>
2019-07-21 23:13:08 +0900

    snippet model

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,13 @@
 class Snippet < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 32 }
+  validates :description, length: { maximum: 128 }
+  validates :file_type, presence: true
+  before_save do
+    file_type.save!
+  end
+
+  validates :content, presence: true
+
 end

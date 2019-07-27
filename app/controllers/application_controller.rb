c61d100f8600cf5f92bbda56f2d6f672c185d4b4
petitviolet <violethero0820@gmail.com>
2019-07-27 23:37:32 +0900

    Changed ApplicationController base to API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+class ApplicationController < ActionController::API
+  rescue_from(ActiveRecord::RecordInvalid) do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 end

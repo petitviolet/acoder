f33ee336c845885fc0680ea05e42b4b2be472969
petitviolet <violethero0820@gmail.com>
2019-07-27 23:39:03 +0900

    Added :user_authenticate!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::API
+  using Sessions
+  include Sessions
+
+  attr_reader :current_user
+
   rescue_from(ActiveRecord::RecordInvalid) do |e|
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def user_authenticate!
+    @current_user ||= request.current_user
+    render json: { error: 'You have to login' }, status: :unauthorized if @current_user.nil?
   end
 end

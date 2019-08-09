3fb5287a1bbd662ca8d44deabed782dafd0eaf99
petitviolet <violethero0820@gmail.com>
2019-08-09 23:11:08 +0900

    a bit more error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::API
   using Sessions
   include Sessions
+  include ApplicationHelper
 
   before_action :user_authenticate
   attr_reader :current_user
 
   rescue_from(ActiveRecord::RecordInvalid) do |e|
+    logger.error("ValidationError: #{e.inspect}")
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  rescue_from(Exception) do |e|
+    logger.error("Error: #{e.inspect}")
+    render json: { error: e.message }, status: :internal_server_error
   end
 
   private

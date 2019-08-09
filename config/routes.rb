18343154995157b9611f9ea96306e2e170da9e7a
petitviolet <violethero0820@gmail.com>
2019-08-09 23:16:58 +0900

    added HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :snippets
 
+  root 'home#index'
   get '/whoami', to: 'sessions#whoami'
   post '/login', to: 'sessions#login'
   delete '/logout', to: 'sessions#logout'

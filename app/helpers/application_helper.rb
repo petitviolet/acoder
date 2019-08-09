9d6af0d53ed03ccf18f01518d5f0544558ad1919
petitviolet <violethero0820@gmail.com>
2019-08-09 23:10:52 +0900

    Added logger method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  # @return [Logger]
+  def logger
+    Rails.logger
+  end
+
+  def with_logging(msg:, options: {})
+    logger.info { "[Start]#{msg}" }
+    res = yield
+    logger.info { "[End]#{msg}#{res if options[:result]}" }
+    res
+  end
+
+  def with_time_logging(msg:, options: {})
+    logger.info { "[Start]#{msg}" }
+    start = Time.now.utc
+    res = yield
+    logger.info { "[End][#{Time.now.utc - start} sec]#{msg}#{res if options[:result]}" }
+    res
+  end
 end

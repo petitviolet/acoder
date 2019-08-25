a21ca55cbc97a8f511654cf2c0cdba5bc2e88ad4
petitviolet <violethero0820@gmail.com>
2019-08-26 00:01:11 +0900

    development API_HOST

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+process.env.API_HOST = "http://localhost:3000";
+
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()

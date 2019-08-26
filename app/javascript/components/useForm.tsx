f754405b8ca3fcafaca0af0bbbd6b782eac333f6
petitviolet <violethero0820@gmail.com>
2019-08-26 23:50:05 +0900

    Recreate useForm

--- app/javascript/components/useForm.tsx
+++ app/javascript/components/useForm.tsx
@@ -1,23 +1,64 @@
 import * as React from "react";
 
-const useForm = <State extends {}>(onSubmit, initial: State) => {
-    const [values, setValues] = React.useState<State>(initial);
+export interface Validator<State extends {}> {
+    runAll(state: State): Errors
+}
 
-    const handleSubmit = (event) => {
+type Errors = Map<string, string>;
+
+export const useForm = <State extends {}>(onSubmit, initial: State, validator: Validator<State>) => {
+    const [state, setState] = React.useState<State>(initial);
+    const [errors, setErrors] = React.useState<Errors>(new Map());
+    const [disable, setDisable] = React.useState<boolean>(true);
+    const [isEditing, setIsEditing] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        if (isEditing) {
+            setDisable(!isValid());
+        }
+    }, [isEditing, errors]);
+
+    const isValid: () => boolean = React.useCallback(() => {
+        console.dir(errors);
+        return errors.size == 0;
+    }, [errors]);
+
+    const handleSubmit = React.useCallback((event) => {
         if (event) event.preventDefault();
-        onSubmit();
-    };
 
-    const handleChange = (event) => {
-        event.persist();
-        setValues(values => ({...values, [event.target.name]: event.target.value}));
-    };
+        if (!disable && isValid()) {
+            onSubmit(state);
+        } else {
+            console.log(`submit is disabled. error = ${errors}`)
+        }
+
+    }, [state, errors, disable]);
+
+    const handleChange = React.useCallback((event) => {
+        setIsEditing(true);
+        const name: string = event.target.name;
+        const value: string = event.target.value;
+        setState(prevState => {
+            const newState = {...prevState, [name]: value};
+            const errors = Array.from(validator.runAll(newState)).reduce<Map<string, string>>((map, [key, error]) => {
+                if (error) { // kill undefined or null
+                    return map.set(key, error);
+                } else {
+                    return map;
+                }
+            }, new Map());
+            setErrors(errors);
+
+            return newState;
+        });
+
+    }, [validator, state]);
 
     return {
+        state,
+        disable,
         handleChange,
         handleSubmit,
-        values,
     };
 };
 
-export default useForm;

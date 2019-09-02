31fe59d2734f6f4e06eafedd4c299f08d21fe643
petitviolet <violethero0820@gmail.com>
2019-09-02 08:01:45 -0700

    Route with Auth.Component

--- app/javascript/components/Routes.tsx
+++ app/javascript/components/Routes.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Login from "components/Login";
-import Auth from "components/Auth";
+import * as Auth from "components/Auth";
 import UserComponent from "components/User";
 import {FlashComponent} from "components/Flash";
 
@@ -9,15 +9,15 @@ const Routes: React.FC = () => {
     return (
         <div>
             <div>Header</div>
-            <FlashComponent />
+            <FlashComponent/>
             <Switch>
-                <Route exact path={"/"} render={() => ("Home!!!")}/>
-                <Route path={"/login"} component={Login}/>
-                <Auth>
+                <Auth.Component>
+                    <Route exact path={"/"} render={() => ("Home!!!")}/>
+                    <Route path={"/login"} component={Login}/>
                     <Switch>
-                        <Route exact path={"/me"} component={(props) => UserComponent(props.currentUser)} />
+                        <Route exact path={"/me"} component={(props) => UserComponent(props.currentUser)}/>
                     </Switch>
-                </Auth>
+                </Auth.Component>
             </Switch>
         </div>
     );

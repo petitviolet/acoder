d0e418c90fbe977fab75c761c0de6932c9835d97
petitviolet <violethero0820@gmail.com>
2019-09-05 01:03:30 -0700

    UserGateway for user API

---
+++ app/javascript/gateways/UserGateway.ts
@@ -0,0 +1,25 @@
+import Token from "../models/Token";
+import Gateway from "./Gateway";
+import User from "models/User";
+
+class UserGateway extends Gateway {
+
+    login(email: string, password: string): Promise<Token> {
+        return this.axios.post<{ token: Token }>('/login', {
+            email: email,
+            password: password,
+        }).then((res) => {
+            this.responseLogging('login', res);
+            return res.data.token;
+        });
+    }
+
+    currentUser(token: Token) {
+        return this.axios.get<User>('/whoami', this.defaultOptions(token))
+            .then((res) => {
+                this.responseLogging('currentUser', res);
+                return res.data;
+            });
+    }
+}
+export default (token: Token | null = null) => new UserGateway(token);

b4cf5450b51936e1f518e4cf611cef3793f1c3b5
petitviolet <violethero0820@gmail.com>
2019-09-05 01:03:05 -0700

    MyPageComponent

---
+++ app/javascript/components/MyPage.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as Auth from "./Auth";
+
+const MyPageComponent = () => {
+    const {authState} = React.useContext(Auth.Context);
+    const user = authState.currentUser;
+
+    return <div>
+        <div>
+            <div>id:</div>
+            <div>{user.id}</div>
+        </div>
+        <div>
+            <div>name:</div>
+            <div>{user.name}</div>
+        </div>
+        <div>
+            <div>email:</div>
+            <div>{user.email}</div>
+        </div>
+    </div>;
+};
+
+export default MyPageComponent;

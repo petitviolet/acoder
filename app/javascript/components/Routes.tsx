aa3a8e31b143edcbc2f3a7972d02483308a7d8d1
petitviolet <violethero0820@gmail.com>
2019-10-06 23:30:39 +0900

    Added some routes

--- app/javascript/components/Routes.tsx
+++ app/javascript/components/Routes.tsx
@@ -5,6 +5,8 @@ import * as Auth from "components/Auth";
 import UserComponent from "components/User";
 import { FlashComponent } from "components/Flash";
 import MyPageComponent from "components/MyPage";
+import SnippetComponent from "components/Snippet";
+import SnippetListComponent from "components/SnippetList";
 
 const Routes: React.FC = () => {
   return (
@@ -17,6 +19,9 @@ const Routes: React.FC = () => {
           <Route path={"/login"} component={Login} />
           <Switch>
             <Route exact path={"/me"} component={MyPageComponent} />
+            <Route exact path={"/users/:id"} component={UserComponent} />
+            <Route exact path={"/snippets"} component={SnippetListComponent} />
+            <Route exact path={"/snippets/:id"} component={SnippetComponent} />
             {/*<Route*/}
             {/*  path={"/users/:id"}*/}
             {/*  component={(props: {currentUser: Auth.Context}) => UserComponent(props.currentUser)}*/}

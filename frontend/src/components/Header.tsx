d6bc849f3d16c3b18d58d9da10e73b7ee37df2bc
petitviolet <violethero0820@gmail.com>
2019-10-14 20:53:09 +0900

    styled Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,56 @@
 import * as React from 'react';
-import { Redirect } from 'react-router-dom';
 import style from 'styled-components';
+import * as Auth from './Auth';
 
 const Header = () => {
-  return <div></div>;
+  const {
+    authState: { currentUser, token },
+    loggedIn,
+  } = React.useContext(Auth.Context);
+
+  return (
+    <HeaderContainer>
+      <HeaderLink>Home</HeaderLink>
+      <HeaderLink>About</HeaderLink>
+      <HeaderSession>{loggedIn ? 'Log out' : 'Log in'}</HeaderSession>
+    </HeaderContainer>
+  );
 };
 
-const headerContainer = style.div`
-    
+export default Header;
+
+const HeaderContainer = style.header`    
+  overflow: hidden;
+  background-color: #e9e9e9;
+  margin-bottom: 0.5em;
+`;
+
+const HeaderLink = style.a`
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  
+  &:hover {
+    background-color: #ddd;
+    color: black;
+  }
+  &.active {
+    background-color: #2196F3;
+    color: white;
+  }
+`;
+
+const HeaderSession = style.div`
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
 `;

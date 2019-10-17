c93fe50fb2fe03d2949b5c2c2a727cdc91c0a6c0
petitviolet <violethero0820@gmail.com>
2019-10-17 22:25:39 +0900

    Added timestamps to Snippet

--- frontend/src/models/Snippet.ts
+++ frontend/src/models/Snippet.ts
@@ -5,15 +5,20 @@ class Snippet {
   readonly fileType: string;
   readonly description: string;
   readonly content: object;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
 
-  constructor(id: string, userId: string, title: string, fileType: string, description: string, content: object) {
+  constructor(id: string, userId: string, title: string, fileType: string, description: string, content: object, createdAt: Date, updatedAt: Date) {
     this.id = id;
     this.userId = userId;
     this.title = title;
     this.fileType = fileType;
     this.description = description;
     this.content = content;
+    this.createdAt = new Date(createdAt);
+    this.updatedAt = new Date(updatedAt);
   }
+
 }
 
 export default Snippet;

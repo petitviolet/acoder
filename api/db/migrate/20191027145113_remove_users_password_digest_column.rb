8f7b763ef214323a551ecc2e4d055d99e8d96619
petitviolet <violethero0820@gmail.com>
2019-10-27 23:53:17 +0900

    Remove users.password_digest column for divise

---
+++ api/db/migrate/20191027145113_remove_users_password_digest_column.rb
@@ -0,0 +1,5 @@
+class RemoveUsersPasswordDigestColumn < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :password_digest, :string
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_140340) do
+ActiveRecord::Schema.define(version: 2019_10_27_145113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_10_22_140340) do
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
-    t.string "password_digest"
     t.string "password_salt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

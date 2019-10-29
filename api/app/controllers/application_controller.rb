6c14102f7ec26ad21c70fb56cd1c7cf72e850e48
petitviolet <violethero0820@gmail.com>
2019-10-29 22:16:48 +0900

    Initialized devise_token_auth

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
   include SessionsHelper
   include ApplicationHelper
   before_action :configure_permitted_parameters, if: :devise_controller?

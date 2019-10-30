207dc352cd32207e5aef0d76f2483ff10af3bf93
petitviolet <violethero0820@gmail.com>
2019-10-30 22:51:50 +0900

    Handle authentication tokens with single string

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ApplicationHelper
 
+  before_action :split_tokens
+  prepend_after_action :join_tokens
   before_action :authenticate_user!, unless: :devise_token_auth_controller?
 
   respond_to :json
@@ -31,6 +33,27 @@ class ApplicationController < ActionController::API
       return nil if current_account.nil?
 
       @current_user ||= User.find_by(account_id: current_account.id)
+    end
+
+    def split_tokens
+      return if request.headers['X-Access-Token'].nil?
+
+      token = JSON.parse(Base64.decode64(CGI.unescape(request.headers['X-Access-Token'])))
+      request.headers['access-token'] = token['access-token']
+      request.headers['client'] = token['client']
+      request.headers['uid'] = token['uid']
+    end
+
+    def join_tokens
+      return if request.headers['access-token'].nil?
+
+      auth_json = {
+        'access-token' => request.headers['access-token'],
+        'client' => request.headers['client'],
+        'uid' => request.headers['uid'],
+      }
+      response.headers.delete_if{|key| auth_json.include? key}
+      response.headers['X-Access-Token'] = CGI.escape(Base64.encode64(JSON.dump(auth_json)))
     end
 
     def devise_token_auth_controller?

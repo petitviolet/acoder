c9780c6bc50433badcb83b052d975e5f8c6d1341
petitviolet <violethero0820@gmail.com>
2019-11-01 07:56:18 +0900

    customized sign_up path

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '*page', to: 'home#index', constraints: ->(req) do
+  get '*page', to: 'home#index', constraints: lambda { |req|
     !req.xhr? && req.format.html?
-  end
+  }
 
   scope :api do
-    mount_devise_token_auth_for 'Account', at: 'accounts'
+    mount_devise_token_auth_for 'Account', at: 'accounts', skip: [:registrations]
+    devise_scope :account do
+      post '/accounts/sign_up', to: 'devise_token_auth/registrations#create'
+      put '/accounts', to: 'devise_token_auth/registrations#update'
+      delete '/accounts', to: 'devise_token_auth/registrations#destroy'
+    end
   end
+
   namespace :api, defaults: { format: :json } do
     resources :users
+
     resources :snippets, only: %i[index show create update delete] do
       collection do
         get 'search'

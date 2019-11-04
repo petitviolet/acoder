f98f41615333ecb0d4c667ca4b10be4901e51698
petitviolet <violethero0820@gmail.com>
2019-11-04 21:51:38 +0900

    Added CORS header

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins '*'
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Per-Page', 'Total', 'Link']
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: %w[X-Access-Token]
   end
 end

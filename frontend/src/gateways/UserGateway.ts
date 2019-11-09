de2e296763079e2e4e5ff08d06cbcffc3bb10f41
petitviolet <violethero0820@gmail.com>
2019-11-09 23:50:09 +0900

    Implemented UserGateway#sign_up

--- frontend/src/gateways/UserGateway.ts
+++ frontend/src/gateways/UserGateway.ts
@@ -3,6 +3,38 @@ import Gateway from './Gateway';
 import User from '../models/User';
 
 class UserGateway extends Gateway {
+  signUp(name: string, email: string, password: string): Promise<{ user: User }> {
+    return this.axios
+      .post<User>('/users/sign_up', {
+          user: {
+            name: name,
+            email: email,
+            password: password,
+          },
+      })
+      .then(res => {
+        this.responseLogging('sign_up', res);
+        return { user: res.data };
+      })
+      .catch(error => {
+        const errorMsg = (() => {
+          if (!error.response) {
+            return error;
+          }
+          if (error.response.status == 422) {
+            return error.response.data.error; // error message
+          } else if (error.response.status >= 500) {
+            console.log(`Server Error: ${JSON.stringify(error.response.data)}`);
+            return 'Server Error';
+          } else {
+            return JSON.stringify(error.response.data);
+          }
+        })();
+
+        return Promise.reject(errorMsg);
+      });
+  }
+
   login(email: string, password: string): Promise<{ token: Token; user: User }> {
     return this.axios
       .post<User>('/users/sign_in', {

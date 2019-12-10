8d08eb835a904315fb7b300b5962340f560d46ce
petitviolet <violethero0820@gmail.com>
2019-12-10 21:49:30 +0900

    Added some queries

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -9,5 +9,26 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :current_user, Types::UserType, null: false, description: 'fetch current user'
+
+    field :user, Types::UserType, null: true, description: 'fetch user by id' do
+      argument :id, String, required: true
+    end
+
+    field :snippets, [Types::SnippetType], null: false, description: 'fetch snippets' do
+    end
+
+    def current_user
+      context[:current_user]
+    end
+
+    def user(id:)
+      User.find_by(id: id)
+    end
+
+    def snippets
+      Snippet.feed
+    end
   end
 end

b5557a6349d042702ba95ee3009f6c8f4f41ae3c
petitviolet <violethero0820@gmail.com>
2019-12-10 21:49:23 +0900

    Add current_user to GraphQL context

--- api/app/controllers/graphql_controller.rb
+++ api/app/controllers/graphql_controller.rb
@@ -9,10 +9,9 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user,
     }
-    result = AcoderSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = GraphqlSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
     raise e unless Rails.env.development?

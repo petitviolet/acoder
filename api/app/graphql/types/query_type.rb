0e87bf43b83725028f45a88b1880df423e81f1c7
petitviolet <violethero0820@gmail.com>
2019-12-10 21:54:27 +0900

    Removed auto-generated code from query_type

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -1,15 +1,5 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-
     field :current_user, Types::UserType, null: false, description: 'fetch current user'
 
     field :user, Types::UserType, null: true, description: 'fetch user by id' do

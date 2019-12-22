c89be025d03d3de54d363cc67016844e620676b2
petitviolet <violethero0820@gmail.com>
2019-12-22 23:56:47 +0900

    Modified cors settings

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins /localhost\:\d+/, /acoder-frontend\.herokuapp\.com/
 
     resource '*',
              headers: :any,
